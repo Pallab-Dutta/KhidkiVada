5e59e920c9148fdc1a4e3498f25dd8f4a4781ded
Antant-Boutique <antant.boutique@gmail.com>
2025-06-22 01:21:10 +0530

    update

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const API_BASE_URL = 'http://172.28.107.66:5000';
+    const API_BASE_URL = 'http://127.0.0.1:5000';
     const appContainer = document.getElementById('app-container');
     const globalNav = document.getElementById('global-nav');
     const bottomNav = document.getElementById('bottom-nav');
